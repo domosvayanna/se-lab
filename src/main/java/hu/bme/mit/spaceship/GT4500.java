3806465f92d2590a8ca0ba349890a23f919b392b
Domosvay Annamaria <domosvay.annamaria@edu.bme.hu>
2023-09-27 15:15:23 +0200

    fixed

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -77,6 +77,8 @@ public class GT4500 implements SpaceShip {
         break;
 
       case ALL:
+        firingSuccess = primaryTorpedoStore.fire(1);
+        firingSuccess = secondaryTorpedoStore.fire(1);
         // try to fire both of the torpedo stores
         //TODO implement feature
 
