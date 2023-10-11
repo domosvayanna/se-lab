2eeac1c01713975a999f751fb68e8d6910cdd5a5
Domosvay Annamaria <domosvay.annamaria@edu.bme.hu>
2023-10-11 15:53:13 +0200

    added throw in public boolean fire()

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -79,8 +79,6 @@ public class GT4500 implements SpaceShip {
       case ALL:
         firingSuccess = primaryTorpedoStore.fire(1);
         firingSuccess = secondaryTorpedoStore.fire(1);
-        // try to fire both of the torpedo stores
-        //TODO implement feature
 
         break;
     }
